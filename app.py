429b21d3560b1b804707b0080ec7f90b42c3bbfa
arou-ssya <aroussiayouneb14@gmail.com>
2025-08-16 01:10:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(app)  # Autorise les requêtes depuis Flutter
 
 # Configuration du modèle
-MODEL_URL = "https://drive.google.com/uc?id=12zWlffsA0K6czhT1ohELJVN7UoT7AXpH"
+MODEL_URL = "https://drive.google.com/uc?export=download&confirm=t&id=12zWlffsA0K6czhT1ohELJVN7UoT7AXpH"
 MODEL_PATH = "alzheimer_model_float32.tflite"
 
 # Télécharger le modèle au démarrage
@@ -66,3 +66,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
